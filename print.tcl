dbc184dc776dc9371ea8b5d0b2031ad6fe1e1fe7
devmichalek <devmichalek@gmail.com>
2018-12-24 15:02:32 +0100

    Printing functions

---
+++ print.tcl
@@ -0,0 +1,76 @@
+
+namespace eval Supp {}
+
+proc Supp::PrintTitle {} {
+	puts "MINE\t\t\t    ENEMY\n"
+}
+
+proc Supp::PrintDouble {a b max} {
+
+	# ". A B C D E F G H I J...  |  . A B C D E F G H I J..."
+	set add 65
+	set str ". "
+	for {set i $add} {$i < $max + $add} {incr i} {
+		append str "[format %c $i $i] "
+	}
+	append str "  |   . "
+	for {set i $add} {$i < $max + $add} {incr i} {
+		append str "[format %c $i $i] "
+	}
+	puts $str
+	set str ""
+
+	for {set i 0} {$i < $max} {incr i} {
+
+		# 0...9
+		append str $i
+		append str " "
+
+		# data
+		for {set j 0} {$j < $max} {incr j} {
+			append str [lindex $a $i $j]
+			append str " "
+		}
+
+		# gap
+		append str "  |   "
+
+		# 0...9
+		append str $i
+		append str " "
+
+		# data
+		for {set j 0} {$j < $max} {incr j} {
+			append str [lindex $b $i $j]
+			append str " "
+		}
+
+		puts $str
+		set str ""
+	}
+}
+
+proc Supp::PrintLabel {actionstr} {
+	if {$Sets::CharMode} {
+		puts "\nS - intact part of ship"
+		puts "X - drown part of ship"
+		puts "0 - mishit part of ship"
+	}
+
+	if {$Sets::ActionMode} {
+		puts "\nCurrent Action: $actionstr"
+	}
+}
+
+proc Supp::PrintGap {rows} {
+	for {set i 0} {$i < $rows} {incr i} {
+		puts ""
+	}
+}
+
+proc Supp::Print {a b max actionstr} {
+	eval exec >&@stdout <@stdin [auto_execok cls]
+	PrintTitle
+	PrintDouble $a $b $max
+	PrintLabel $actionstr
+}
